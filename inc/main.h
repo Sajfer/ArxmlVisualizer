0ffa881ed2ef000bcb5deb158a501889805ebe80
Anton Bretting <Anton.bretting@arccore.com>
2018-08-20 12:39:25 +0200

    Now take model path as argument

--- inc/main.h
+++ inc/main.h
@@ -1,11 +1,18 @@
 #include <wx/wx.h>
+#include <wx/cmdline.h>
+
 #include "drawpane.h"
 
 class ArxmlVizualizer : public wxApp
 {
   public:
     virtual bool OnInit();
+    virtual int OnRun();
+    virtual void OnInitCmdLine(wxCmdLineParser& parser);
+    virtual bool OnCmdLineParsed(wxCmdLineParser& parser);
   private:
     wxFrame* frame;
     DrawPane* drawPane;
+
+    std::string filePath;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,16 @@
 
 IMPLEMENT_APP(ArxmlVizualizer)
 
-bool ArxmlVizualizer::OnInit()
-{
+static const wxCmdLineEntryDesc cmdLineDesc[] = {
+    { wxCMD_LINE_PARAM,  NULL, NULL, "input file", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_MULTIPLE },
+    { wxCMD_LINE_NONE }
+};
+
+
+bool ArxmlVizualizer::OnInit() {
+
+    if (!wxApp::OnInit())
+        return false;
 
     wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
     frame = new wxFrame((wxFrame*) NULL, -1, wxT("Arxml Vizualizer"), wxPoint(50,50), wxSize(800,600));
@@ -17,7 +25,36 @@ bool ArxmlVizualizer::OnInit()
     frame->SetAutoLayout(true);
     frame->Show();
 
-    Arxml arxml("Model.arxml");
+    
+
+    return true;
+}
+
+int ArxmlVizualizer::OnRun() {
+    Arxml arxml(filePath);
+
+    return 0;
+}
+
+void ArxmlVizualizer::OnInitCmdLine(wxCmdLineParser& parser) {
+    parser.SetDesc (cmdLineDesc);
+    // must refuse '/' as parameter starter or cannot use "/path" style paths
+    parser.SetSwitchChars (wxT("-"));
+}
+
+bool ArxmlVizualizer::OnCmdLineParsed(wxCmdLineParser& parser) {
+
+    // to get at your unnamed parameters use
+    wxArrayString files;
+    for (uint i = 0; i < parser.GetParamCount(); i++)
+    {
+        files.Add(parser.GetParam(i));
+    }
+
+    if(files.GetCount() == 0) {
+        return false;
+    }
+    filePath = files.Item(0);
 
     return true;
 }
