6a78148dc0a93f24b9486bedb7eecdf6a39639db
Axel Goteman <axel.goteman@arccore.com>
2018-08-23 14:25:38 +0200

    cleanup

--- src/communications_panel.cpp
+++ src/communications_panel.cpp
@@ -1,7 +1,7 @@
 #include "communication_panel.h"
 #define BACKGROUND_COLOR wxColor(220, 220, 220)
 
-CommunicationsPanel::CommunicationsPanel(wxWindow* parent): wxPanel(parent, COMMUNICATION_PANEL_ID, wxPoint(0,0), wxSize(100,100)) { //, "", wxPoint(0,0),wxSize(100, 100)) {
+CommunicationsPanel::CommunicationsPanel(wxWindow* parent): wxPanel(parent, COMMUNICATION_PANEL_ID) {
     SetBackgroundColour(BACKGROUND_COLOR);
 };
 
